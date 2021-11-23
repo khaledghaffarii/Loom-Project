00419b997e4899f421a94ebf4190aa9f9548e924
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-23 14:33:55 +0100

    style: upload the theme color config in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#3490dc",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+        blue:"#23212d"
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
