ce19f34a3115519d5fd037b4c6774e5817216b5c
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-23 14:34:35 +0100

    style:change the className Style bg color all Home Page in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from "next/head";
-import Header from '../Components/Header';
+import Header from "../Components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-blue h-screen ">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
-      <Header/>
+      <Header />
     </div>
   );
 }
