c05325c2d0cf1e60c69d7ce4fb7ca1acb395ca96
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-11-23 18:08:54 +0100

    fix:import to asset folder and implimented the Image in <Image/> from next/image in `Header`Component

--- Components/Header.js
+++ Components/Header.js
@@ -1,13 +1,20 @@
-import { Image } from 'next/image';
+import  Image  from "next/image";
+import Images from '../assets/loomLogo.webp'
 function Header() {
   return (
     <div className="flex justify-between  max-w-6xl mx-5 xl:mx-auto">
-      <Image
-        src="../assets/loomLogo.webp"
-        layout="fill"
-        objectFit="contain"
-        priority={false}
-      />
+      <div
+        onClick={() => router.push("/")}
+        className="relative w-24 hidden lg:inline-grid cursor-pointer "
+      >
+        {" "}
+        <Image
+          src={Images}
+          layout="fill"
+          objectFit="contain"
+          priority={false}
+        />
+      </div>
     </div>
   );
 }
