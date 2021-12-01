b81f07de826ad485efb683d7a7ef896f9237c6c0
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-12-01 16:37:34 +0100

    refactor: import header in index file

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Carousel from "../Components/Carousel";
 import Header from "../Components/Header";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Carousel />
     </div>
   );
 }
