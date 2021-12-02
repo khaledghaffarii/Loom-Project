cc11854b87fae2ed0afb8ba3fd912cc9fcfd7ed4
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-12-02 16:18:38 +0100

    refactor: import Publicity component in INdex file

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Head from "next/head";
 import Carousel from "../Components/Carousel";
 import Header from "../Components/Header";
+import Publicity from "../Components/Publicity";
 
 export default function Home() {
   return (
-    <div className="bg-blue h-screen ">
+    <div className="bg-blue">
       <Head>
         <title>Loom</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-      <Carousel />
+      <Header/>
+      <Carousel/>
+      <Publicity/>
     </div>
   );
 }
