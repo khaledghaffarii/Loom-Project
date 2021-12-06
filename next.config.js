345564a08f997010570908074066fff7ec240892
khaledghaffarii <med.khaledghaffary@gmail.com>
2021-12-06 18:29:20 +0100

    style :remove className to image in Publicity Components

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   images: {
-    domains: ["links.papareact.com"],
+    domains: [
+      "https://scontent.ftun2-1.fna.fbcdn.net/v/t1.15752-9/263488892_294067899309648_4914377171344657843_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=frz8Tw9-bYUAX9krZte&_nc_ht=scontent.ftun2-1.fna&oh=ef199438e2fadc7a1d83c2963a98a9d6&oe=61D31D97",
+    ],
   },
 };
